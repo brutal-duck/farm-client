9afa4b48fd34c304f2a13e1623b1b3678a1db332
Vladimir <vsrodionov94@ya.ru>
2021-01-27 10:21:31 +0700

    set build name 3.3.1

--- src/scripts/scenes/Boot.ts
+++ src/scripts/scenes/Boot.ts
@@ -39,7 +39,7 @@ class Boot extends Phaser.Scene {
 
   public init(): void {
     
-    let build: string = '3.3';
+    let build: string = '3.3.1';
     console.log('Build ' + build);
 
     // автосохранение при уходе с игры
