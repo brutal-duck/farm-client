d6fcfe1197c472feeb2dcf89a466d983542108ee
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-02-04 13:31:04 +0700

    add bar

--- src/scripts/general/boosts.ts
+++ src/scripts/general/boosts.ts
@@ -1,11 +1,24 @@
 function showFeedTime(): void {
     
-    let x: number = this.cameras.main.centerX
-    let y: number = this.cameras.main.centerY - 200;
+    let x: number = this.cameras.main.centerX;
+    let y: number = this.cameras.main.centerY - 250;
+
+    
     let text: Phaser.GameObjects.Text = this.add.text(x, y,  this.state.lang.feedBoostNative + this.shortTime(this.state[`user${this.state.farm}`].feedBoostTime, this.state.lang), {
-        font: '31px Shadow',
+        font: '35px Bip',
         fill: '#FFFFFF'
-      }).setOrigin(0.5, 0.5).setDepth(y).setStroke('#000000', 5);
+      });
+    text.setOrigin(0.5, 0.5)
+      .setDepth(y)
+      .setDepth(y + 1)
+      .setStroke('#000000', 5);
+
+    let textWidth: number = text.width + 50;
+
+    let bar: Phaser.GameObjects.Graphics = this.add.graphics();
+    bar.fillStyle(0x000000, 0.6)
+      .fillRoundedRect(x - textWidth / 2, y - 35, textWidth, 70, 20)
+      .setDepth(y);
 
     let alpha: number = 1;
 
@@ -16,11 +29,13 @@ function showFeedTime(): void {
 
           if (alpha >= 0) {
 
-            alpha -= 0.02;
+            alpha -= 0.015;
             text.setAlpha(alpha);
+            bar.setAlpha(alpha);
 
           } else {
 
+            bar.destroy();
             text.destroy();
             timer.remove();
 
