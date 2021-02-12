c8ab2315a0e6d45c964012e2f72a316042aaa37e
Mayr-Katarn <70950158+Mayr-Katarn@users.noreply.github.com>
2021-02-12 16:19:07 +0700

    upd

--- src/scripts/scenes/Modal/Chat/Main.ts
+++ src/scripts/scenes/Modal/Chat/Main.ts
@@ -17,6 +17,7 @@ class Chat extends Phaser.Scene {
   public maxNameWordLength: number;
   public textWrap: number;
   public lastMsgFromUser: string;
+  public userName: string;
 
   public msg: any;
 
@@ -29,6 +30,7 @@ class Chat extends Phaser.Scene {
     this.maxNameWordLength = 450
     this.textWrap = 340;
     this.lastMsgFromUser;
+    this.userName = 'user1'
 
 
     this.msg = [
@@ -48,10 +50,21 @@ class Chat extends Phaser.Scene {
         text: '3j4t-9ytut tje dh\n93jgvm3'
       },
       {
+        name: 'user3',
+        time: '22.22.2222',
+        text: '3h\n93jgvm3'
+      },
+      {
         name: 'user4',
         time: '22.22.2222',
-        text: '3j4t-9 93jgvm3 34jfg3 gj3pg wwg3t3t 3geaga a g4'
+        text: '3j4t-9 93jgvm3 34jfg3 gj3pg wwg3t3t 3geaga a gwe234444444444442343242234444444244 24234234'
       },
+      {
+        name: 'user1',
+        time: '22.22.2222',
+        text: 'yeryr'
+      },
+
     ]
   }
 
@@ -111,9 +124,6 @@ class Chat extends Phaser.Scene {
     this.scrollHeight += 100
 
     this.buildMessages()
-    this.buildMessages()
-    // this.gettedMessage()
-    // this.gettedMessage()
 
     this.scrolling.bottom = this.scrollHeight
     this.scrolling.scrollY =  this.scrollHeight
@@ -127,167 +137,176 @@ class Chat extends Phaser.Scene {
 
   
   public buildMessages(): void {
-    // ИСХОДЯЩЕЕ СООБЩЕНИЕ
+
+    for (let i in this.msg) {
+      
+      if (this.msg[i].name === this.userName) {
+
+        // СООБЩЕНИЯ ПОЛЬЗОВАТЕЛЯ
+        let padding: number = 32
+        if (this.lastMsgFromUser !== this.msg[i].name) padding = 8
     
-
-    // Текст исходящего сообщения
-    let outputText: Phaser.GameObjects.Text = this.add.text(this.windowWidth - this.textWrap - 24, this.windowHeight + this.scrollHeight + 10, this.msg[1].text + 'kjlk', {
-      font: '19px Shadow',
-      color: '#FADAC1',
-      align: 'left',
-      wordWrap: { width: this.textWrap }
-    })
-    .setOrigin(0, 0)
-    .setCrop(0, 0, this.textWrap - 5, 100)
-    .setDepth(1)
+        // Текст исходящего сообщения
+        let outputText: Phaser.GameObjects.Text = this.add.text(this.windowWidth - this.textWrap - 24, this.windowHeight + this.scrollHeight + padding, this.msg[i].text + 'kjlk', {
+          font: '19px Shadow',
+          color: '#FADAC1',
+          align: 'left',
+          wordWrap: { width: this.textWrap }
+        })
+        .setOrigin(0, 0)
+        .setCrop(0, 0, this.textWrap - 5, 100)
+        .setDepth(1)
+        
+        let textHeight: number = outputText.getBounds().height
+        let textWidth: number = outputText.getBounds().width
+        
     
-    let textHeight: number = outputText.getBounds().height
-    let textWidth: number = outputText.getBounds().width
-    // console.log(textHeight, textWidth)
+        // Текст Ника
+        if (this.lastMsgFromUser !== this.msg[i].name) {
     
-    // Текст Ника
-    let nicknameTextWidth: number
-    // let nicknameTextHeight: number = 0
-    let padding: number = 36
-    // if (this.lastMsgFromUser !== this.msg[1].name) {
-      // padding = 56
-      let nicknameText: Phaser.GameObjects.Text = this.add.text(32, outputText.y - 34, this.msg[1].name, {
-        font: '18px Shadow',
-        color: '#63527F',
-        align: 'right'
-      })
-      .setOrigin(0, 0)
-      .setCrop(0, 0, this.windowWidth - 40, 100)
-      .setDepth(1)
-      nicknameTextWidth = nicknameText.getBounds().width
-      // nicknameTextHeight = nicknameText.getBounds().height
-      if (nicknameTextWidth < 479) nicknameText.setX(this.windowWidth - nicknameTextWidth - 10)
-    // }
-
-    // Время исходящего сообщения
-    let timeText: Phaser.GameObjects.Text = this.add.text(this.windowWidth - 24, outputText.y + textHeight + padding, this.msg[1].time, {
-      font: '16px Shadow',
-      color: '#FADAC1',
-      align: 'right'
-    })
-    .setOrigin(1)
-    .setDepth(1)
-
-    let timeHeight: number = timeText.getBounds().height
-    let timeWidth: number = timeText.getBounds().width
+          let nicknameText: Phaser.GameObjects.Text = this.add.text(32, outputText.y - 34, this.msg[i].name, {
+            font: '18px Shadow',
+            color: '#63527F',
+            align: 'right'
+          })
+          .setOrigin(0, 0)
+          .setCrop(0, 0, this.windowWidth - 40, 100)
+          .setDepth(1)
     
+          let nicknameTextWidth: number = nicknameText.getBounds().width
+          if (nicknameTextWidth < 479) nicknameText.setX(this.windowWidth - nicknameTextWidth - 10)
+    
+        }
+    
+    
+        // Время исходящего сообщения
+        let timeText: Phaser.GameObjects.Text = this.add.text(this.windowWidth - 24, outputText.y + textHeight + 36, this.msg[i].time, {
+          font: '16px Shadow',
+          color: '#FADAC1',
+          align: 'right'
+        })
+        .setOrigin(1)
+        .setDepth(1)
+    
+        let timeHeight: number = timeText.getBounds().height
+        let timeWidth: number = timeText.getBounds().width
+        
+    
+        // Определяем размер и положение плашки и текста
+        let bgX: number = outputText.x - 14
+        let bgWidth: number = textWidth
+        let bgRound: number = 24
+    
+        if (textWidth > 364) bgWidth = 336
+        else {
+          outputText.setX(this.windowWidth - textWidth - 24)
+          bgX = outputText.x - 14
+        }
+        // Минимальный размер и положение плашки
+        if (textWidth < timeWidth - 10) {
+          timeText.setX(this.windowWidth - 14)
+          // bgWidth = timeWidth
+          // bgX = timeText.x - timeWidth - 14
+          bgRound = 12
+        }
+    
+        // Фон сообщения
+        let outputMsgBg = this.add.graphics()
+        .fillStyle(0x63527F, 1)
+        .fillRoundedRect(bgX, outputText.y - 14 + 4, bgWidth + 28, textHeight + 28, bgRound)
+        let outputMsg = this.add.graphics()
+        .fillStyle(0x6162AD, 1)
+        .fillRoundedRect(bgX, outputText.y - 14, bgWidth + 28, textHeight + 28, bgRound)
+        
+        this.lastMsgFromUser = this.msg[i].name
+    
+        // Добавляем длинну скролла
+        this.scrollHeight += textHeight + timeHeight + padding * 2
 
-    // Определяем размер и положение плашки и текста
-    let bgX: number = outputText.x - 14
-    let bgWidth: number = textWidth
-    let bgRound: number = 24
+      } else {
 
-    if (textWidth > 364) bgWidth = 364
-    else {
-      outputText.setX(this.windowWidth - textWidth - 24)
-      bgX = outputText.x - 14
+        // ВХОДЯЩИЕ СООБЩЕНИЯ
+        let padding: number = 34
+        // if (this.lastMsgFromUser !== this.msg[1].name) padding = 20
+        
+        // Текст сообщения
+        let gettedText: Phaser.GameObjects.Text = this.add.text(24, this.windowHeight + this.scrollHeight + padding, this.msg[i].text, {
+          font: '19px Shadow',
+          color: '#5E340C',
+          align: 'left',
+          wordWrap: { width: this.textWrap }
+        })
+        .setOrigin(0)
+        .setCrop(0, 0, this.textWrap - 5, 100)
+        .setDepth(1)
+        
+        let textHeight: number = gettedText.getBounds().height
+        let textWidth: number = gettedText.getBounds().width
+      
+        // Текст Ника
+        // if (this.lastMsgFromUser !== this.msg[0].name) {
+      
+          let nicknameText: Phaser.GameObjects.Text = this.add.text(10, gettedText.y - 34, this.msg[i].name, {
+            font: '18px Shadow',
+            color: '#FADAC1',
+            align: 'right'
+          })
+      
+          .setOrigin(0)
+          .setCrop(0, 0, this.windowWidth - 40, 100)
+          .setDepth(1)    
+      
+        // }
+      
+        // Время сообщения
+        let timeText: Phaser.GameObjects.Text = this.add.text(24, gettedText.y + textHeight + 20, this.msg[i].time, {
+          font: '16px Shadow',
+          color: '#FADAC1',
+          align: 'right'
+        })
+        .setOrigin(0)
+        .setDepth(1)
+      
+        let timeHeight: number = timeText.getBounds().height
+        let timeWidth: number = timeText.getBounds().width
+        
+      
+        // Определяем плашки и текст
+        let bgX: number = gettedText.x - 14
+        let bgWidth: number = textWidth
+        let bgRound: number = 24
+      
+        if (textWidth > 364) bgWidth = 364
+        // Устанавливает минимальный размер и положение плашки
+        if (textWidth < timeWidth - 10) {
+          timeText.setX(14)
+          // bgWidth = timeWidth
+          // bgX = timeText.x - timeWidth - 14
+          bgRound = 12
+        }
+      
+        // Фон сообщения
+        let gettedMsgBg = this.add.graphics()
+        .fillStyle(0x63527F, 1)
+        .fillRoundedRect(bgX, gettedText.y - 14 + 4, bgWidth + 28, textHeight + 28, bgRound)
+        let gettedMsg = this.add.graphics()
+        .fillStyle(0xFADAC1, 1)
+        .fillRoundedRect(bgX, gettedText.y - 14, bgWidth + 28, textHeight + 28, bgRound)
+      
+        this.lastMsgFromUser = this.msg[i].name
+        
+        // Добавляем длинну скролла
+        this.scrollHeight += textHeight + timeHeight + padding * 2
+
+        
+      }
+
+
     }
-    // Устанавливает минимальный размер и положение плашки
-    if (textWidth < timeWidth - 10) {
-      timeText.setX(this.windowWidth - 14)
-      // bgWidth = timeWidth
-      // bgX = timeText.x - timeWidth - 14
-      bgRound = 12
-    }
 
-    // Фон сообщения
-    let outputMsgBg = this.add.graphics()
-    .fillStyle(0x63527F, 1)
-    .fillRoundedRect(bgX, outputText.y - 14 + 4, bgWidth + 28, textHeight + 28, bgRound)
-    let outputMsg = this.add.graphics()
-    .fillStyle(0x6162AD, 1)
-    .fillRoundedRect(bgX, outputText.y - 14, bgWidth + 28, textHeight + 28, bgRound)
-    
-    this.lastMsgFromUser = this.msg[1].name
-
-    // Добавляем длинну скролла
-    this.scrollHeight += textHeight + timeHeight + 14
     
   }
 
-
-
-
-
-  gettedMessage(): void {
-    // ИСХОДЯЩЕЕ СООБЩЕНИЕ
-    // Текст Ника
-    if (this.lastMsgFromUser !== this.msg[0].name) {
-
-      let nicknameText: Phaser.GameObjects.Text = this.add.text(10, this.windowHeight + this.scrollHeight - 24, this.msg[0].name, {
-        font: '18px Shadow',
-        color: '#FADAC1',
-        align: 'right'
-      })
-      .setOrigin(0)
-      .setCrop(0, 0, this.windowWidth - 40, 100)
-      .setDepth(1)    
-
-    }
-    
-
-    // Текст сообщения
-    let gettedText: Phaser.GameObjects.Text = this.add.text(24, this.windowHeight + this.scrollHeight + 10, this.msg[0].text + 'kj4lk', {
-      font: '19px Shadow',
-      color: '#5E340C',
-      align: 'left',
-      wordWrap: { width: this.textWrap }
-    })
-    .setOrigin(0)
-    .setCrop(0, 0, this.textWrap - 5, 100)
-    .setDepth(1)
-
-    let textHeight: number = gettedText.getBounds().height
-    let textWidth: number = gettedText.getBounds().width
-    // console.log(textHeight, textWidth)
-
-
-    // Время сообщения
-    let timeText: Phaser.GameObjects.Text = this.add.text(24, gettedText.y + textHeight + 20, this.msg[0].time, {
-      font: '16px Shadow',
-      color: '#FADAC1',
-      align: 'right'
-    })
-    .setOrigin(0)
-    .setDepth(1)
-
-    let timeHeight: number = timeText.getBounds().height
-    let timeWidth: number = timeText.getBounds().width
-    
-
-    // Определяем плашки и текст
-    let bgX: number = gettedText.x - 14
-    let bgWidth: number = textWidth
-    let bgRound: number = 24
-
-    if (textWidth > 364) bgWidth = 364
-    // Устанавливает минимальный размер и положение плашки
-    if (textWidth < timeWidth - 10) {
-      timeText.setX(14)
-      // bgWidth = timeWidth
-      // bgX = timeText.x - timeWidth - 14
-      bgRound = 12
-    }
-
-    // Фон сообщения
-    let gettedMsgBg = this.add.graphics()
-    .fillStyle(0x63527F, 1)
-    .fillRoundedRect(bgX, gettedText.y - 14 + 4, bgWidth + 28, textHeight + 28, bgRound)
-    let gettedMsg = this.add.graphics()
-    .fillStyle(0xFADAC1, 1)
-    .fillRoundedRect(bgX, gettedText.y - 14, bgWidth + 28, textHeight + 28, bgRound)
-
-    this.lastMsgFromUser = this.msg[0].name
-    
-    // Добавляем длинну скролла
-    this.scrollHeight += textHeight + timeHeight + 34
-
-  }
 
 }
 
