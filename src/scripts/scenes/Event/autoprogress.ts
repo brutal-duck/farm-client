8f144f6e56ca13fafcb6f2f4d1ae270882c0fa4f
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-02-19 12:11:42 +0700

    add new sprite money package and refact autoprogress

--- src/scripts/scenes/Event/autoprogress.ts
+++ src/scripts/scenes/Event/autoprogress.ts
@@ -28,249 +28,162 @@ function autoprogress(load: boolean = false): void {
     this.state.userEvent.collector = 0;
   }
 
-  // процент шерсти под бустом
-  let feedPercent: number = Number((wasFeedBoost / wasCollector).toFixed(2));
-  if (feedPercent >= 1 ) feedPercent = 1;
+  // процент ресурсов под бустом
+  let feedBoostNumberPercent: number = 100 + Number((wasFeedBoost / wasCollector).toFixed(2)) * 100;
+  if (feedBoostNumberPercent >= 200 ) feedBoostNumberPercent = 200;
+  let feedPercent: bigint = BigInt(feedBoostNumberPercent);
 
   if (!load) this.game.scene.keys['EventBars'].collector.update();
   if (!load) this.state.timeToHerdBoost -= this.state.offlineTime;
   
-  // считаем сколько снесла курица яйцо
-  // let balance: Ibalance = this.balance();
-  // let newEggs: { egg: boolean, id: string, type: number, count: number }[] = [];
+  // считаем сколько появилось ресурсов
+  let newResources: { resource: boolean, id: string, type: number, count: number }[] = [];
 
-  // for (let i in this.animals.children.entries) {
+  for (let i in this.animals.children.entries) {
 
-  //   let animal = this.animals.children.entries[i];
-  //   let breed: number;
+    let animal = this.animals.children.entries[i];
+    let breed: number = animal.data.values.type;
 
-  //   if (animal.type === 0) breed = 1;
-  //   else breed = animal.data.values.type;
+    let points: IeventPoints = this.state.eventSettings.eventSettings.find((item: IeventPoints) => item.breed === breed);
 
-  //   let points: IchickenPoints = this.settings.chickenSettings.find((item: IchickenPoints) => item.breed === breed);
+    let resource: number = points.resource;
 
-  //   let egg: number = points.egg;
+    let resources: number = Math.floor((resource * wasCollector) / 1000);
 
-  //   if (balance.alarm) {
-  //     egg = Math.round(egg / 100 * this.settings.chickenBadPercent);
-  //     if (egg < 1) egg = 1;
-  //   }
-
-  //   let eggs: number = Math.floor((egg * wasCollector) / 1000);
-
-  //   if (eggs === 0) {
-  //     if (animal.egg + (eggs * wasCollector) > 1000) animal.egg = 1000;
-  //     else animal.egg += (eggs * wasCollector);
-  //   }
+    if (resources === 0) {
+      if (animal.data.values.active.data.values.resource + (resources * wasCollector) > 1000) animal.data.values.active.data.values.resource = 1000;
+      else animal.data.values.active.data.values.resource += (resources * wasCollector);
+    }
     
-  //   if (this.state.userEvent.collector === 0) {
-  //     animal.egg = random(0, 1000);
-  //   }
+    if (this.state.userEvent.collector === 0) {
+      animal.data.values.active.data.values.resource = random(0, 1000);
+    }
 
-  //   newEggs.push({
-  //     id: animal._id,
-  //     type: animal.type,
-  //     count: eggs,
-  //     egg: false
-  //   });
+    if (animal.data.values.active.data.values.working) {
+      newResources.push({
+        id: animal.data.values._id,
+        type: animal.data.values.type,
+        count: resources,
+        resource: false
+      });
+    }
+  }
 
-  // }
+  // добавляем в массив ресурсов, которые могут лежать на поле
+  for (let i in this.resources.children.entries) {
 
-  // // добавляем в массив яйца, которые могут лежать на поле
-  // for (let i in this.eggs.children.entries) {
+    let resource = this.resources.children.entries[i];
 
-  //   let egg = this.eggs.children.entries[i];
+      newResources.push({
+        id: resource.data.values._id,
+        type: resource.data.values.type,
+        count: 1,
+        resource: true
+      });
 
-  //   if (egg.type !== 0) {
+  }
 
-  //     newEggs.push({
-  //       id: egg._id,
-  //       type: egg.type,
-  //       count: 1,
-  //       egg: true
-  //     });
+  // скорость сборки
+  let percent: number = 100;
+  let speed: number = this.state.eventCollectorSettings.find((data: IcollectorSettings) => data.level === this.state.userEvent.collectorLevel).speed;
 
-  //   }
+  if (this.animals.children.entries.length > speed * 10) {
 
-  // }
-
-  // // берем кристаллические курицы если есть и формируем обновленный массив
-  // let eggs: { egg: boolean, id: string, type: number, count: number }[] = [];
-
-  // for (let i in newEggs) {
-
-  //   if (newEggs[i].type !== 0) {
-
-  //     eggs.push(newEggs[i]);
-
-  //   } else {
-
-  //     let animal = this.animals.children.entries.find((data: any) => data._id === newEggs[i].id);
-  //     let territory = this.currentTerritory(animal.x, animal.y);
-
-  //     let count: number = 0;
-  //     let minX: number = (territory.position - 1) * 240 + indent;
-  //     let maxX: number = territory.position * 240 - indent;
-  //     let minY: number = territory.block * 240 + indent;
-  //     let maxY: number = (territory.block + 1) * 240 - indent;
-
-  //     if (newEggs[i].count < animal.diamond) {
-
-  //       animal.diamond -= newEggs[i].count;
-  //       count = newEggs[i].count;
-
-  //     } else {
-
-  //       count = 5 - animal.diamond;
-  //       animal.destroy();
-        
-  //     }
-
-  //     for (let j: number = 0; j < count; j++) {
-        
-  //       let egg: IchickenEgg = {
-  //         type: 0,
-  //         x: random(minX, maxX),
-  //         y: random(minY, maxY),
-  //         _id: 'local_' + randomString(18)
-  //       }
-  
-  //       this.getEgg(egg);
-
-  //     }
-
-  //   }
-
-  // }
-
-  // // скорость сборки
-  // let percent: number = 100;
-  // let speed: number = this.state.chickenCollectorSettings.find((data: IcollectorSettings) => data.level === this.state.userEvent.collectorLevel).speed;
-
-  // if (this.animals.children.entries.length > speed * 10) {
-
-  //   let excess: number = 100 / (speed * 10) * this.animals.children.entries.length;
-  //   percent = Math.round(100 / (excess / 100));
+    let excess: number = 100 / (speed * 10) * this.animals.children.entries.length;
+    percent = Math.round(100 / (excess / 100));
     
-  // }
+  }
 
-  // // формируем массив яиц
-  // let eggsArr: { id: string, type: number }[] = [];
+  // формируем массив ресурсов
+  let resourceArr: { id: string, type: number }[] = [];
 
-  // for (let i in eggs) {
-  //   for (let j: number = 0; j < eggs[i].count; j++) {
-  //     eggsArr.push({
-  //       id: eggs[i].id,
-  //       type: eggs[i].type
-  //     });
-  //   }
-  // }
+  for (let i in newResources) {
+    for (let j: number = 0; j < newResources[i].count; j++) {
+      resourceArr.push({
+        id: newResources[i].id,
+        type: newResources[i].type
+      });
+    }
+  }
 
-  // // ложим яйца в хранилища
-  // let length: number = eggsArr.length;
-  // if (percent < 100) length = Math.floor(eggsArr.length / 100 * percent);
+  // сохраняем ресурсы
+  let length: number = resourceArr.length;
+  if (percent < 100) length = Math.floor(resourceArr.length / 100 * percent);
 
-  // for (let i: number = 0; i < length; i++) {
+  for (let i: number = 0; i < length; i++) {
     
-  //   let price: number = this.state.chickenSettings.chickenSettings.find((data: IchickenPoints) => data.breed === eggsArr[i].type).eggPrice;
-  //   price *= (1 + feedPercent); // коэфф
+    let price: bigint = this.state.eventSettings.eventSettings.find((data: IeventPoints) => data.breed === resourceArr[i].type).resourcePrice;
+    price = (price * feedPercent) / BigInt(100); // коэфф
     
-  //   for (let j in this.territories.children.entries) {
-
-  //     if (this.territories.children.entries[j].type === 5) {
-        
-  //       let territory = this.territories.children.entries[j];
-
-  //       let max: number = this.settings.territoriesChickenSettings.find((item: IterritoriesChickenSettings) => item.improve === territory.improve).eggStorage;
-
-  //       if (territory.volume < max && wasCollector > 0) {
-
-  //         let egg = eggs.find(data => data.id === eggsArr[i].id);
-  //         if (egg.count > 0) egg.count--;
-
-  //         territory.money += price;
-  //         territory.volume++;
-  //         break;
-
-  //       }
-        
-  //     }
+    if (newResources.length < this.maxCountResource && wasCollector > 0) {
       
-  //   }
+      let resource = newResources.find(data => data.id === resourceArr[i].id);
+      if (resource.count > 0) resource.count--;
 
-  // }
+      this.state.userEvent.money += price;
+      break;
 
-  // // убираем яйца, которые собрали с поля
-  // for (let i in eggs) {
+    }
+    
 
-  //   if (eggs[i].egg && eggs[i].count === 0) {
-  //     let egg = this.eggs.children.entries.find((data: any) => data._id === eggs[i].id);
-  //     egg.destroy();
-  //   }
+  }
 
-  // }
+  // убираем ресурсы, которые собрали с поля
+  for (let i in newResources) {
 
-  // // ложим остатки яиц на поле
-  // let remainingEggs: number[] = [];
-  // for (let i in eggs) {
-  //   for (let j: number = 0; j < eggs[i].count; j++) {
-  //     if (!eggs[i].egg) {
-  //       remainingEggs.push(eggs[i].type);
-  //     }
-  //   }
-  // }
+    if (newResources[i].resource && newResources[i].count === 0) {
+      
+      let resource = this.resources.children.entries.find((data: any) => data.data.values._id === newResources[i].id);
+      
+      resource.destroy();
+    }
 
-  // // формируем свободные места на клетках
-  // let freeSpace: Iposition[] = [];
-  // for (let i in this.territories.children.entries) {
+  }
 
-  //   let territory = this.territories.children.entries[i];
+  // ложим остатки ресурсов на поле
+  let remainingResources: number[] = [];
+  for (let i in newResources) {
+    console.log(newResources[i])
+    for (let j: number = 0; j < newResources[i].count; j++) {
+      console.log(newResources[i])
+      if (!newResources[i].resource) {
+        
+        remainingResources.push(newResources[i].type);
+      }
+    }
+  }
 
-  //   if (territory.type === 2 || territory.type === 3) {
-
-  //     let count: number = this.settings.territoriesChickenSettings.find((item: IterritoriesChickenSettings) => item.improve === territory.improve).countEggs;
-  //     let minX: number = (territory.position - 1) * 240 + indent;
-  //     let maxX: number = territory.position * 240 - indent;
-  //     let minY: number = territory.block * 240 + indent;
-  //     let maxY: number = (territory.block + 1) * 240 - indent;
-
-  //     for (let j in this.eggs.children.entries) {
-  //       let egg = this.eggs.children.entries[j];
-  //       if (egg.x > minX && egg.x < maxX && egg.y > minY && egg.y < maxY) count--;
-  //     }
-
-  //     for (let j: number = 0; j < count; j++) {
-  //       freeSpace.push({
-  //         x: random(minX, maxX),
-  //         y: random(minY, maxY)
-  //       });
-  //     }
-
-  //   }
-
-  // }
+  // формируем свободные места на рабочей зоне
+  let freeSpace: Iposition[] = [];
+  for (let j: number = 0; j < this.maxCountResource; j++) {
+    freeSpace.push({
+      x: random(20, 700),
+      y: random(this.topIndent + 20, this.topIndent + 460)
+    });
+  }
   
-  // // ставим оставшиеся яйца на поле
-  // let index: number = 0;
-  // for (let i in freeSpace) {
+  // ставим оставшиеся ресуры на поле
+  let index: number = 0;
+  for (let i in freeSpace) {
 
-  //   if (typeof remainingEggs[index] === 'number') {
+    if (typeof remainingResources[index] === 'number') {
 
-  //     let egg: IchickenEgg = {
-  //       type: remainingEggs[index],
-  //       x: freeSpace[i].x,
-  //       y: freeSpace[i].y,
-  //       _id: 'local_' + randomString(18)
-  //     }
+      let resource: IchickenEgg = {
+        type: remainingResources[index],
+        x: freeSpace[i].x,
+        y: freeSpace[i].y,
+        _id: 'local_' + randomString(18)
+      }
 
-  //     this.getEgg(egg);
+      this.getResource(resource);
 
-  //   } else break;
+    } else break;
 
-  //   index++;
+    index++;
 
-  // }
-
+  }
+  console.log('Осталось',remainingResources)
+  console.log('всего',newResources)
 }
 
 export default autoprogress;
