0d1d12bfa803980ac899214021545a87605fab19
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-03-17 09:52:53 +0700

    add interactive starterpackicon in bars

--- src/scripts/scenes/Sheep/SheepBars.ts
+++ src/scripts/scenes/Sheep/SheepBars.ts
@@ -385,6 +385,14 @@ class SheepBars extends Phaser.Scene {
     if (!this.state.user.starterpack) {
       
       this.starterpackIcon = this.add.image(490, 45, 'starterpack-icon').setScale(0.2).setVisible(false);
+      this.click(this.starterpackIcon, ()=>{
+        const modal: Imodal = {
+          type: 2,
+          shopType: 1,
+        }
+        this.state.modal = modal;
+        this.scene.launch('Modal', this.state);
+      })
       
       this.tweens.add({
         targets: this.starterpackIcon,
