ebbba03be01271fd8177956785588800c93a0149
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-04-01 10:17:45 +0700

    create new components

---
+++ src/scripts/components/Egg.ts
@@ -0,0 +1,37 @@
+import Resource from "./Resource";
+import Chicken from './../scenes/Chicken/Main';
+
+export default class Egg extends Resource {
+  public scene: Chicken;
+  private isClicked: boolean;
+  public timeout: number;
+  public _id: string;
+  public animalType: number;
+  
+  constructor(scene: Chicken, { x , y, type, _id }: IchickenEgg) {
+    super(scene, x, y, `chicken-egg${type}`);
+    this.isClicked = false;
+    this.timeout = 0;
+    this.type = String(type);
+    this._id = _id;
+    this.animalType = type;
+    this.setEventClick();
+  }
+
+  static create(scene: Chicken, egg: IchickenEgg): Egg {
+    const newEgg = new Egg(scene, egg);
+    newEgg.scene.eggs.add(newEgg);
+    return newEgg;
+  }
+
+  public setEventClick(): void {
+    this.scene.click(this, () => {
+      if (!this.isClicked) {
+        this.isClicked = true;
+        let manualСollect: boolean = false;
+        if (Number(this.animalType) !== 0) manualСollect = true;
+        this.scene.collectEgg(this, manualСollect);
+      }
+    });
+  }
+}
---
+++ src/scripts/components/Wool.ts
@@ -0,0 +1,15 @@
+import Resource from './Resource';
+import Sheep from './../scenes/Sheep/Main';
+
+export default class Wool extends Resource {
+  public scene: Sheep;
+  constructor(scene: Sheep, { x , y }: Iposition, type: number) {
+    super(scene, x, y, `sheep-wool${type}`);
+
+  }
+  static create(scene: Sheep, position: Iposition, type: number, target: Iposition): Wool {
+    const newWool = new Wool(scene, position, type);
+    newWool.flyToPoint(target);
+    return newWool;
+  }
+}
