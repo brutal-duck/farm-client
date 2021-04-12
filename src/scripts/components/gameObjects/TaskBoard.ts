51c78458276e133b665cd4040e356570c5e00611
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-04-12 16:48:05 +0700

    add task list animation

--- src/scripts/components/gameObjects/TaskBoard.ts
+++ src/scripts/components/gameObjects/TaskBoard.ts
@@ -10,7 +10,7 @@ import CowBars from '../../scenes/Cow/CowBars';
 */
 
 // плашка заданий
-export default class TaskBoard extends Phaser.GameObjects.Graphics{
+export default class TaskBoard extends Phaser.GameObjects.TileSprite{
 
   public scene: SheepBars | ChickenBars | CowBars;
   public isVisibile: boolean;
@@ -39,11 +39,17 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
   public isUpdated: boolean = false;
   public taskId: number;
   public gotAward: number;
+  public dY: number;
+  public taskListElements: any[] = [];
+  public listButton: Phaser.GameObjects.TileSprite;
+  public listButtonText: Phaser.GameObjects.Text;
+  public listIsOpen: boolean = false;
+  public listButtondY: number = 0;
 
   constructor(
     scene: SheepBars | ChickenBars | CowBars
   ) {
-    super(scene)
+    super(scene, 0, 0, 0, 0, 'white-pixel')
     this.scene = scene;
     this.isVisibile = false;
     this.init();
@@ -55,6 +61,7 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
 
   private init(): void {
     this.scene.add.existing(this);
+    this.setOrigin(0);
 
     this.positionY = this.scene.height;
     this.createElements();
@@ -140,12 +147,17 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
     }).setDepth(1).setOrigin(0.5, 0.5).setVisible(false);
 
     this.taskProgress = this.scene.add.graphics().setVisible(false);
+
+    this.listButton = this.scene.add.tileSprite(0,0, 0, 0, 'white-pixel').setTint(0xFFEBC5).setOrigin(1).setVisible(false);
+    this.listButtonText = this.scene.add.text(0, 0, '^', {
+      font: '34px Bip',
+      color: '#00000',
+    }).setOrigin(1).setVisible(false);
   }
 
   public preUpdate(): void {
 
     this.checkVisibility();
-
     let stateParts: Ipart[];
     let userData: IuserSheep | IuserChicken | IuserCow;
     let countBreed: number;
@@ -211,7 +223,6 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
     }
   
     if (!this.isUpdated) {
-      this.clear();
       this.awardBg?.clear();
       this.taskProgress?.clear();
 
@@ -237,10 +248,9 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
         if (height < 110) height = 110;
 
         this
-          .clear()
           .setPosition(30, this.scene.height - 190 - height)
-          .fillStyle(0xFFEBC5, 1)
-          .fillRoundedRect(0, 0, 660, height, 8);
+          .setDisplaySize(660, height)
+          .setTint(0xFFEBC5);
 
         this.taskIcon
           .setTexture(taskData.icon)
@@ -277,6 +287,11 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
           .strokePath()
           .setDepth(3);
           
+        this.listButton.setPosition(this.tileSprite.getBounds().right, this.tileSprite.getBounds().top).setDisplaySize(20, 20).setOrigin(1);
+        this.listButtonText.setPosition(this.listButton.x, this.listButton.y).setOrigin(1, 0.5);
+        this.scene.click(this.listButton, () => {
+          this.toggleList();
+        });
       } else if (this.status === 2 && task) {
       
         const taskData: ItaskData = this.scene.game.scene.keys[this.scene.state.farm].getTaskData(task);
@@ -318,8 +333,8 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
       
         this
           .setPosition(30, this.positionY - 190 - height)
-          .fillStyle(0xFFEBC5, 1)
-          .fillRoundedRect(0, 0, 660, height, 8);
+          .setDisplaySize(660, height)
+          .setTint(0xFFEBC5);
         
         this.taskIcon
           .setTexture(taskData.icon)
@@ -355,8 +370,8 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
       } else if (this.status === 3 && task) {
         this
           .setPosition(30, this.positionY - 300)
-          .fillStyle(0xFFEBC5, 1)
-          .fillRoundedRect(0, 0, 660, 110, 8);
+          .setDisplaySize(660, 110)
+          .setTint(0xFFEBC5);
       
         this.doneButton.setPosition(this.scene.cameras.main.centerX, this.positionY - 245);
 
@@ -383,8 +398,8 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
 
         this
           .setPosition( 30, this.scene.height - 300)
-          .fillStyle(0xFFEBC5, 1)
-          .fillRoundedRect(0, 0, 660, 110, 8);
+          .setDisplaySize(660, 110)
+          .setTint(0xFFEBC5);
       
         this.lastPart.setPosition(this.scene.cameras.main.centerX, this.scene.height - 245);
 
@@ -483,6 +498,8 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
     this.doneButtonText.setY(this.doneButtonText.y + dYanim);
     this.lastPart.setY(this.lastPart.y + dYanim);
     this.taskProgress.setY(this.taskProgress.y + dYanim);
+    this.listButton.setY(this.listButton.y + dYanim);
+    this.listButtonText.setY(this.listButtonText.y + dYanim);
   }
 
   private flyInMainBoardAnim(): void {
@@ -506,6 +523,8 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
         this.doneButtonText,
         this.lastPart,
         this.taskProgress,
+        this.listButton,
+        this.listButtonText
       ],
       alpha: { from: 0, to: 1 },
       y: '-=250',
@@ -551,15 +570,16 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
 
     if ((this.scene.menu.isOpened ||
       this.scene.scene.isActive('Modal') ||
-      this.scene.scene.isActive('Tutorial')) &&
+      this.scene.scene.isActive('Tutorial') || this.listIsOpen) &&
       this.isVisibile || !checkSheepTutor && this.isVisibile) {
       this.hideAllElement();
+      this.hideListButton();
     } else if (!this.scene.menu.isOpened &&
       !this.scene.scene.isActive('Modal') &&
       !this.scene.scene.isActive('Tutorial') &&
-      !this.isVisibile && checkSheepTutor) {
+      !this.isVisibile && checkSheepTutor && !this.listIsOpen) {
         this.shownElements();
-      }
+    } 
   }
 
   private hideAllElement(): void {
@@ -582,6 +602,10 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
     this.taskProgress?.setVisible(false);
   }
 
+  private hideListButton(): void {
+    this.listButton?.setVisible(false);
+    this.listButtonText?.setVisible(false);
+  }
   private shownElements(): void {
     this.hideAllElement();
     this.isVisibile = true;
@@ -594,6 +618,8 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
       this.doneText?.setVisible(true);
       this.taskText?.setVisible(true);
       this.taskProgress?.setVisible(true);
+      this.listButton?.setVisible(true);
+      this.listButtonText?.setVisible(true);
     } else if (this.status === 2) {
       this.setVisible(true);
       this.taskText.setVisible(true);
@@ -624,5 +650,174 @@ export default class TaskBoard extends Phaser.GameObjects.Graphics{
   private removeButtonsInteractive(): void {
     this.doneButton.removeInteractive();
     this.done.removeInteractive();
+  }
+
+  private openAllTasksWindow(): void {
+    this.hideAllElement();
+    this.setdY();
+    this.moveListButtonTop();
+    this.createAllTasks();
+  }
+
+  private setdY(): void {
+    const tasksParams: Itasks[] = this.scene.game.scene.keys[this.scene.state.farm].partTasks();
+    tasksParams.sort((x1: Itasks, x2: Itasks) => {
+      if (x1.got_awarded < x2.got_awarded) return -1;
+      if (x1.got_awarded > x2.got_awarded) return 1;
+      if (x1.done < x2.done) return 1;
+      if (x1.done > x2.done) return -1;
+      if (x1.sort < x2.sort) return -1;
+      if (x1.sort > x2.sort) return 1;
+      return 0;
+    });
+    this.dY = 0;
+
+    for (let i: number = 0; i < tasksParams.length; i += 1) {    
+      const task: Itasks = tasksParams[i];
+      const taskData: ItaskData = this.scene.game.scene.keys[this.scene.state.farm].getTaskData(task);
+      const icon: Phaser.GameObjects.Image = this.scene.add.sprite(0, 0, taskData.icon).setVisible(false).setScale(0.9);
+      this.dY += (icon.height + 20);
+    }
+    this.listButtondY = this.dY - 105;
+  }
+
+  private createAllTasks(): void {
+    const tasksParams: Itasks[] = this.scene.game.scene.keys[this.scene.state.farm].partTasks();
+    tasksParams.sort((x1: Itasks, x2: Itasks) => {
+      if (x1.got_awarded > x2.got_awarded) return -1;
+      if (x1.got_awarded < x2.got_awarded) return 1;
+      if (x1.done > x2.done) return 1;
+      if (x1.done < x2.done) return -1;
+      if (x1.sort > x2.sort) return -1;
+      if (x1.sort < x2.sort) return 1;
+      return 0;
+    });
+
+    let last: boolean = false;
+    for (let i: number = 0; i < tasksParams.length; i += 1) {    
+      const task: Itasks = tasksParams[i];
+      const taskData: ItaskData = this.scene.game.scene.keys[this.scene.state.farm].getTaskData(task);
+      if (i === tasksParams.length - 1) last = true;
+      this.createTaskListElement(task, taskData,  last);
+    }
+  }
+
+  private createTaskListElement(task: Itasks, taskData: ItaskData, last: boolean): void {
+    let icon: Phaser.GameObjects.Image = this.scene.add.sprite(88, this.positionY - 250, taskData.icon).setDepth(1).setScale(0.9).setAlpha(0);
+    let text: Phaser.GameObjects.Text = this.scene.add.text(icon.getBounds().right + 20, this.positionY - 250, taskData.name, {
+      font: '24px Bip',
+      color: '#944000',
+      align: 'left',
+      wordWrap: { width: 460 }
+    }).setDepth(1).setOrigin(0, 0.5).setAlpha(0);
+    let height = icon.height + 20;
+
+    const bgTile: Phaser.GameObjects.TileSprite = this.scene.add.tileSprite(this.scene.cameras.main.centerX, this.positionY - 250, 660, height, 'white-pixel');
+    bgTile.setTint(0xFFEBC5).setInteractive();
+    let lineTile: Phaser.GameObjects.TileSprite = null;
+    if (!last) {
+      lineTile = this.scene.add.tileSprite(this.scene.cameras.main.centerX, bgTile.y + bgTile.height / 2, 640, 3, 'white-pixel').setDepth(1).setAlpha(0);
+      lineTile.setTint(0x858585);
+    }
+
+    let completed: Phaser.GameObjects.Sprite = null;
+    if (task.done === 1 && task.got_awarded === 1) {
+      icon.setTint(0x777777).setAlpha(0);
+      completed = this.scene.add.sprite(icon.x, icon.y, 'completed').setDepth(1).setTint(0xc0c0c0).setOrigin(0.5, 0.5).setAlpha(0);
+      text.setColor('#494949').setAlpha(0);
+    }    
+    this.dY -= (height);
+    console.log(this.dY);
+    const targets: any[] = [ icon, text, lineTile, completed,  bgTile ];
+    this.taskListElements = [...this.taskListElements, targets];
+    this.openTaskListAnimation(targets)
+  }
+
+  private openTaskListAnimation(targets: any[]): void {
+    let target: any[] = [...targets]; 
+    const timeline: Phaser.Tweens.Timeline = this.scene.tweens.timeline();
+    timeline.add({
+      targets: targets,
+      duration: 700,
+      y: `-=${this.dY}`,
+      ease: 'Power1', 
+    });
+    target.pop();
+    timeline.add({
+      targets: target,
+      alpha: { from: 0, to: 1},
+      duration: 300,
+    });
+    timeline.play();
+    
+  }
+
+  private closeTaskListAnimation(): void {
+    this.taskListElements.reverse();
+    this.taskListElements.forEach((array: any[], index)=> {
+      const timeline: Phaser.Tweens.Timeline = this.scene.tweens.timeline();
+      const targets = [...array];
+      this.dY = (array[0].height + 20) * index; 
+      targets.pop();
+      timeline.add({
+        targets: targets,
+        alpha: { from: 1, to: 0},
+        duration: 300,
+      });
+      timeline.add({
+        targets: array,
+        duration: 700,
+        y: `+=${this.dY}`,
+        ease: 'Power1', 
+        onComplete: (): void => {
+          console.log('complete')
+          array.forEach(el => {
+            el?.destroy();
+          })
+        }
+      });
+      timeline.play();
+    });
+    this.scene.time.addEvent({
+      delay: 300,
+      callback: (): void => {
+        this.moveListButtonButtom();
+      }
+    });
+    this.scene.time.addEvent({
+      delay: 950,
+      callback: (): void => {
+        this.listIsOpen = false;
+      }
+    })
+  }
+  
+  private toggleList(): void {
+    if (!this.listIsOpen) {
+      this.listIsOpen = true;
+      this.listButtonText.setFlipY(true);
+      this.openAllTasksWindow();
+    } else {
+      this.listButtonText.setFlipY(false);
+      this.closeTaskListAnimation();
+    }
+  }
+
+  private moveListButtonTop(): void {
+    this.scene.tweens.add({
+      targets: [ this.listButton, this.listButtonText ],
+      y: `-=${this.listButtondY}`,
+      duration: 700,
+      ease: 'Power1',
+    })
+  }
+
+  private moveListButtonButtom(): void {
+    this.scene.tweens.add({
+      targets: [ this.listButton, this.listButtonText ],
+      y: `+=${this.listButtondY}`,
+      duration: 700,
+      ease: 'Power1',
+    })
   }
 }
--- src/scripts/scenes/Sheep/SheepPreload.ts
+++ src/scripts/scenes/Sheep/SheepPreload.ts
@@ -332,7 +332,7 @@ let firework3: any = require("./../../../assets/images/animations/firework3.png"
 let fireworkBg: any = require("./../../../assets/images/animations/fireworkBg.png");
 let loadingSpinner: any = require('./../../../assets/images/animations/loading-spinner.png');
 let farmer: any = require("./../../../assets/images/farmer.png");
-
+let whitePixel: any = require("./../../../assets/images/white-pixel.jpg");
 
 class SheepPreload extends Phaser.Scene {
 
@@ -703,7 +703,7 @@ class SheepPreload extends Phaser.Scene {
     this.load.image('fireworkBg', fireworkBg);
     this.load.image('loading-spinner', loadingSpinner);
     this.load.image('farmer', farmer);
-
+    this.load.image('white-pixel', whitePixel);
   }
   
   
