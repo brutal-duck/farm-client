a223956780c5e3f9d560146ffd522dff77ae8980
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-10-26 17:31:20 +0700

    fix amplitude

--- src/scripts/libs/Amplitude.ts
+++ src/scripts/libs/Amplitude.ts
@@ -44,9 +44,10 @@ export default class Amplitude {
         .set('partner', this.state.platform)
         .set('test', this.state.user.test)
         .set(`Catcher${this.state.farm}`, this.state[`user${this.state.farm}`].collectorLevel);
+        
       if (this.state.platform === 'ok' || this.state.platform === 'vk') {
         let refer: string = '';
-        location.search.substr(1).split('&').forEach(function (item) {
+        location.search.substr(1).split('&').forEach(item => {
           const [key, value] = item.split('=');
           if (
             this.state.platform === 'vk' && key === 'referrer' 
