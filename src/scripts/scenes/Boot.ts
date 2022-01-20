4222f9e7a381d143594d0476b23499dc2cf08ef9
Vladimir Rodionov <vsrodionov94@yandex.ru>
2022-01-20 14:44:39 +0700

    set new build 4.19

--- src/scripts/scenes/Boot.ts
+++ src/scripts/scenes/Boot.ts
@@ -68,7 +68,7 @@ class Boot extends Phaser.Scene {
   public getPlatformStorage = getPlatformStorage.bind(this);
 
   public init(): void {
-    this.build = 4.18;
+    this.build = 4.19;
     // console.log(this.game.device, 'this.game.device');
     this.state = state;
     this.fontsReady = false;
